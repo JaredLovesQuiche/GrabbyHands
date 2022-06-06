5f114a3fc9b689a62c47cff088924e4489ee6b21
Jared <jvaun20@gmail.com>
2022-06-06 00:45:51 -0400

    Added simple camera and movement controls.

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,17 @@ public class PlayerController : MonoBehaviour, PlayerControls.IPlayerActions
 {
     private PlayerControls controls;
     private Camera playerCam;
+    private Rigidbody rb;
 
     public Transform body;
+
+    private Vector2 mouseDelta = Vector2.zero;
+    private float rotX = 0f;
+    private float rotY = 0f;
+
+    private Vector2 move = Vector2.zero;
+    public float acceleration = 8f;
+    public float maxSpeed = 3f;
 
     private void Start()
     {
@@ -16,6 +25,11 @@ public class PlayerController : MonoBehaviour, PlayerControls.IPlayerActions
         controls.Player.Enable();
 
         playerCam = Camera.main;
+
+        rb = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void Update()
@@ -23,25 +37,46 @@ public class PlayerController : MonoBehaviour, PlayerControls.IPlayerActions
         Debug.DrawRay(body.position, body.forward, Color.blue);
     }
 
-    private void FixedUpdate()
+    private void LateUpdate()
     {
-        Debug.Log(playerCam.transform.forward);
+        MouseLook();
     }
 
-    private void MoveInDirection()
+    private void FixedUpdate()
     {
+        MovePlayerCharacter();
+    }
 
+    private void MovePlayerCharacter()
+    {
+        Vector3 moveDirection = (move.y * transform.forward + move.x * transform.right).normalized;
+        rb.AddForce(moveDirection * acceleration);
+        if (rb.velocity.magnitude > maxSpeed) rb.velocity = rb.velocity.normalized * maxSpeed;
+    }
+
+    private void MouseLook()
+    {
+        float sensitivity = 0.1f;
+
+        float minX = -80f;
+        float maxX = 80f;
+
+        rotY += mouseDelta.x * sensitivity;
+        rotX += -mouseDelta.y * sensitivity;
+        rotX = Mathf.Clamp(rotX, minX, maxX);
+
+        transform.localEulerAngles = new Vector3(0f, rotY, 0f);
+        playerCam.transform.localEulerAngles = new Vector3(rotX, 0f, 0f);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-
+        move = context.ReadValue<Vector2>();
     }
 
     public void OnLook(InputAction.CallbackContext context)
     {
-        Vector2 delta = context.ReadValue<Vector2>();
-        print(delta);
+        mouseDelta = context.ReadValue<Vector2>();
     }
 
     public void OnMouse(InputAction.CallbackContext context)
